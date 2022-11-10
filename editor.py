b2ef51ce9cab823f4cdc2616d86955628fe6069e
Cabrikfan <zhenchar@gmail.com>
2022-11-10 14:27:46 +0300

    Вот домашняя работа

--- editor.py
+++ editor.py
@@ -1,1 +1,24 @@
 import pygame
+from setup import *
+
+pygame.init()
+pygame.display.set_caption("Test OOP")
+size = [WIDTH, HEIGHT]
+scene = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+playGame = True
+deltatime = 0
+
+while playGame:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playGame = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                playGame = False
+
+    scene.fill((0, 0, 0))
+
+    pygame.display.flip()
+
+    deltatime = clock.tick(FPS) / 1000
