712ad76f5c8cdd4d89993460875747cf2af58f3f
OrthodoxEgo <vgtrofimov@gmail.com>
2022-11-11 18:12:45 +0300

    Рамки, кратные 5 ячейкам.

--- manager.py
+++ manager.py
@@ -11,26 +11,30 @@ class Manager:
         for i in range(20):
             self.sq.append([])
             for j in range(20):
-                self.sq[i].append(Square(64 + i * setup.SIZE, 64 + j * setup.SIZE, setup.SIZE))
+                self.sq[i].append(Square(setup.BLANK_FIELD + i * setup.SIZE, setup.BLANK_FIELD + j * setup.SIZE, setup.SIZE))
 
     def draw(self, scene: pygame):
         for i in range(len(self.sq)):
             for j in range(len(self.sq[i])):
                 self.sq[i][j].draw(scene)
 
+    def get_xy_coordinate_from_mouse(self, x, y):
+        p1 = (x - setup.BLANK_FIELD) // setup.SIZE
+        p2 = (y - setup.BLANK_FIELD) // setup.SIZE
+        return p1, p2
+
     def click(self, x, y):
-        p1 = (x - 64) // setup.SIZE
-        p2 = (y - 64) // setup.SIZE
-        self.sq[p1][p2].enabled = self.trigger
+        p1, p2 = self.get_xy_coordinate_from_mouse(x, y)
+        if (p1 >= 0 and p1 < len(self.sq) and p2 >= 0 and p2 < len(self.sq[0])):
+            self.sq[p1][p2].enabled = self.trigger
 
     def newMotion(self, x, y):
-        p1 = (x - 64) // setup.SIZE
-        p2 = (y - 64) // setup.SIZE
-        self.trigger = not self.sq[p1][p2].enabled
-
+        p1, p2 = self.get_xy_coordinate_from_mouse(x, y)
+        if (p1 >= 0 and p1 < len(self.sq) and p2 >= 0 and p2 < len(self.sq[0])):
+            self.trigger = not self.sq[p1][p2].enabled
 
     # Вернёт True, если клетка закрашена
     def getEnabled(self, x, y):
-        p1 = (x - 64) // setup.SIZE
-        p2 = (y - 64) // setup.SIZE
-        return self.sq[p1][p2].enabled
+        p1, p2 = self.get_xy_coordinate_from_mouse(x, y)
+        if (p1 >= 0 and p1 < len(self.sq) and p2 >= 0 and p2 < len(self.sq[0])):
+            return self.sq[p1][p2].enabled
--- square.py
+++ square.py
@@ -1,7 +1,9 @@
 import pygame
+import setup
 class Square:
 
     color = (100, 100, 100)
+    color_line = (190, 190, 190)
     color_fill = (170, 170, 170)
 
     def __init__(self, x, y, size):
@@ -11,9 +13,22 @@ class Square:
         self.enabled = False
 
     def draw(self, scene: pygame):
-        if (self.enabled):
-            pygame.draw.rect(scene, Square.color_fill, (self.x + 3, self.y + 3,
-                                                   self.size - 6, self.size - 6))
+        if self.enabled:
+            pygame.draw.rect(scene, Square.color_fill, (self.x + 3, self.y + 3, self.size - 6, self.size - 6))
         else:
-            pygame.draw.rect(scene, Square.color, (self.x, self.y, self.size, self.size), 2)
+            pygame.draw.rect(scene, Square.color, (self.x, self.y, self.size, self.size), 1)
+
+        if ((self.x - setup.BLANK_FIELD) / self.size) % 5 == 0:
+            pygame.draw.line(scene, Square.color_line, (self.x, self.y), (self.x, self.y + self.size), 1)
+        elif ((self.x + self.size - setup.BLANK_FIELD) / self.size) % 5 == 0:
+            pygame.draw.line(scene, Square.color_line, (self.x + self.size, self.y), (self.x + self.size, self.y + self.size), 1)
+
+        if ((self.y - setup.BLANK_FIELD) / self.size) % 5 == 0:
+            pygame.draw.line(scene, Square.color_line, (self.x, self.y), (self.x + self.size, self.y), 1)
+        elif ((self.y + self.size - setup.BLANK_FIELD) / self.size) % 5 == 0:
+            pygame.draw.line(scene, Square.color_line, (self.x, self.y + self.size), (self.x + self.size, self.y + self.size), 1)
+
+
+
+
 
