4160cf06ce7f5d0062f62264879b0e076204299b
OrthodoxEgo <vgtrofimov@gmail.com>
2022-11-22 16:03:17 +0300

    Полный функционал редактора.

--- data/map.py
+++ data/map.py
@@ -3,3 +3,6 @@ class Map:
     def __init__(self, name_level, data_level):
         self.name_level = name_level
         self.data_level = data_level
+
+        if self.name_level == "":
+            raise ValueError('Ошибка: нет имени уровня, работать не буду :(')
