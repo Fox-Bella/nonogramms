c3f7be314d174b37f8f34664c0ad815aac0e5def
OrthodoxEgo <vgtrofimov@gmail.com>
2022-12-01 18:08:27 +0300

    Исправление критической ошибки выхода за поле.

--- engine/screen/start_screen.py
+++ engine/screen/start_screen.py
@@ -1,16 +1,21 @@
 import pygame
+from setup import *
 
 
 class StartScreen:
 
     def __init__(self):
+        self.frame = 0
         self.enabled = True
         self.alpha = 255
         self.img = pygame.image.load("png/skanmatic.png")
 
     def draw(self, scene, deltatime):
 
+        if self.frame < FPS * 1.5:
+            self.frame += 1
+        else:
+            self.alpha -= 255 * deltatime / 2
+
         self.img.set_alpha(self.alpha)
         scene.blit(self.img, (0, 0))
-
-        self.alpha -= 255 * deltatime / 2
